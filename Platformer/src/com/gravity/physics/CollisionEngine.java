7e193fb7f43f58dac1ba93d6f12d1bbb6bb9e3a9
pgruevski <predrag_gruevski@yahoo.com>
2012-10-31 01:53:23 -0400

    Enabled drawing of hitboxes for debugging purposes.

--- Platformer/src/com/gravity/physics/CollisionEngine.java
+++ Platformer/src/com/gravity/physics/CollisionEngine.java
@@ -6,17 +6,17 @@ import com.google.common.collect.Multimap;
 import com.gravity.geom.Rect;
 
 public interface CollisionEngine {
-    
+
     public boolean addCollidable(Collidable collidable, Integer layer, boolean handlesCollisions);
-    
+
     public boolean removeCollidable(Collidable entity);
-    
+
     public void update(float millis);
-    
+
     public Multimap<Collidable, RectCollision> computeCollisions(float time);
-    
+
     public List<RectCollision> checkAgainstLayer(float time, Collidable collidable, Integer layer);
-    
-    public boolean collidesAgainstLayer(float millis, Rect rect, Integer floraLayer);
-    
+
+    public boolean collidesAgainstLayer(float millis, Rect rect, Integer layer);
+
 }
--- Platformer/src/com/gravity/physics/PhysicsFactory.java
+++ Platformer/src/com/gravity/physics/PhysicsFactory.java
@@ -6,9 +6,9 @@ public final class PhysicsFactory {
         // never instantiated
     }
 
-    private static final float DEFAULT_GRAVITY = 1.0f / 1000f;
-    private static final float DEFAULT_REHANDLE_BACKSTEP = -15f;
-    private static final float DEFAULT_OFFSET_GROUND_CHECK = 2f;
+    public static final float DEFAULT_GRAVITY = 1.0f / 1000f;
+    public static final float DEFAULT_REHANDLE_BACKSTEP = -15f;
+    public static final float DEFAULT_OFFSET_GROUND_CHECK = 2f;
 
     public static GravityPhysics createDefaultGravityPhysics(CollisionEngine engine) {
         return new GravityPhysics(engine, DEFAULT_GRAVITY, DEFAULT_REHANDLE_BACKSTEP, DEFAULT_OFFSET_GROUND_CHECK);

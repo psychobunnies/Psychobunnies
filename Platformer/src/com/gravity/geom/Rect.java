35b6311999ca68ff2dfb7abc041c5ba606a2ff5d
pgruevski <predrag_gruevski@yahoo.com>
2012-11-07 23:11:37 -0500

    Fixed residual wall-hanging bug on corners of tiles.

--- Platformer/src/com/gravity/geom/Rect.java
+++ Platformer/src/com/gravity/geom/Rect.java
@@ -9,18 +9,17 @@ import org.newdawn.slick.geom.Vector2f;
 import com.google.common.base.Preconditions;
 
 /**
- * A rectangle with sides parallel to axes.
- * Custom built, because the one in slick has bugs and performance issues.
+ * A rectangle with sides parallel to axes. Custom built, because the one in slick has bugs and performance issues.
  * 
  * @author xiao
  * 
  */
 public class Rect {
     private static final float EPS = 1e-6f;
-    
+
     private final float x, y;
     private final float height, width;
-    
+
     /**
      * Enum representing the corners of the rectangle.
      * <ul>
@@ -33,7 +32,7 @@ public class Rect {
     public static enum Corner {
         TOPLEFT, TOPRIGHT, BOTLEFT, BOTRIGHT;
     }
-    
+
     /**
      * Enum representing the sides of the rectangle.
      * <ul>
@@ -46,7 +45,7 @@ public class Rect {
      */
     public static enum Side {
         TOP, LEFT, BOTTOM, RIGHT;
-        
+
         public static boolean isSimpleSet(EnumSet<Side> set) {
             if (set.size() == 1) {
                 return true;
@@ -65,14 +64,14 @@ public class Rect {
             }
         }
     }
-    
+
     public Rect(Shape rect) {
         x = rect.getMinX();
         y = rect.getMinY();
         width = rect.getWidth();
         height = rect.getHeight();
     }
-    
+
     public Rect(float x, float y, float width, float height) {
         Preconditions.checkArgument(height > 0, "Height must be positive");
         Preconditions.checkArgument(width > 0, "Width must be positive");
@@ -81,34 +80,34 @@ public class Rect {
         this.height = height;
         this.width = width;
     }
-    
+
     public Rect(Rect rect) {
         this.x = rect.x;
         this.y = rect.y;
         this.height = rect.height;
         this.width = rect.width;
     }
-    
+
     public Vector2f getPoint(Corner index) {
         switch (index) {
-            case TOPLEFT:
-                return new Vector2f(x, y);
-            case TOPRIGHT:
-                return new Vector2f(x + width, y);
-            case BOTLEFT:
-                return new Vector2f(x, y + height);
-            case BOTRIGHT:
-                return new Vector2f(x + width, y + height);
-            default:
-                return new Vector2f(x, y);
+        case TOPLEFT:
+            return new Vector2f(x, y);
+        case TOPRIGHT:
+            return new Vector2f(x + width, y);
+        case BOTLEFT:
+            return new Vector2f(x, y + height);
+        case BOTRIGHT:
+            return new Vector2f(x + width, y + height);
+        default:
+            return new Vector2f(x, y);
         }
     }
-    
+
     /** Given a point (px, py), return if the point is in the rectangle */
     public boolean contains(float px, float py) {
         return px >= x && px - x <= width && py >= y && py - y <= height;
     }
-    
+
     /** Return a new Rect scaled by c about the Rectangle's center */
     public Rect scale(float c) {
         //@formatter:off
@@ -116,22 +115,30 @@ public class Rect {
                         width*c, height*c);
         //@formatter:on
     }
-    
+
+    /** Return a new Rect scaled about the Rectangle's center by xScale in the x direction and by yScale in the y direction. */
+    public Rect scale(float xScale, float yScale) {
+        //@formatter:off
+        return new Rect(x + (1 - xScale) * width / 2, y + (1 - yScale) * height / 2, 
+                        width*xScale, height*yScale);
+        //@formatter:on
+    }
+
     /** Returns a new Rect which is this Rect translated by (x, y) */
     public Rect translate(float tx, float ty) {
         return new Rect(x + tx, y + ty, width, height);
     }
-    
+
     /** Returns a new Rect which is this Rect translated by (x, y) */
     public Rect translate(Vector2f v) {
         return new Rect(x + v.x, y + v.y, width, height);
     }
-    
+
     /** Returns the center of the Rect */
     public Vector2f getCenter() {
         return new Vector2f(x + width / 2, y + height / 2);
     }
-    
+
     /** Returns if the specified rectangle intersects with this one. Adjacency does not count. */
     public boolean intersects(Rect other) {
         if ((x >= (other.x + other.width)) || ((x + width) <= other.x)) {
@@ -142,7 +149,7 @@ public class Rect {
         }
         return true;
     }
-    
+
     /**
      * Returns the side of the collision of specified rect with this rectangle. Adjacency does not count.<br>
      * Will return a single side or two adjacent sides indicating a corner unless:
@@ -171,75 +178,75 @@ public class Rect {
         }
         // BIT ORDER: LEFT RIGHT BOTTOM TOP
         switch (bits) {
-            case 0: // 0000
-            case 15: // 1111
-                return EnumSet.allOf(Side.class);
-            case 8: // 1000
-            case 11: // 1011
-                return EnumSet.of(Side.RIGHT);
-            case 4: // 0100
-            case 7: // 0111
-                return EnumSet.of(Side.LEFT);
-            case 2: // 0010
-            case 14: // 1110
-                return EnumSet.of(Side.BOTTOM);
-            case 1: // 0001
-            case 13: // 1101
-                return EnumSet.of(Side.TOP);
-            case 10: // 1010
-                return EnumSet.of(Side.RIGHT, Side.BOTTOM);
-            case 9: // 1001
-                return EnumSet.of(Side.RIGHT, Side.TOP);
-            case 6: // 0110
-                return EnumSet.of(Side.LEFT, Side.BOTTOM);
-            case 5: // 0101
-                return EnumSet.of(Side.LEFT, Side.TOP);
-            case 12: // 1100
-                return EnumSet.of(Side.TOP, Side.BOTTOM);
-            case 3: // 0011
-                return EnumSet.of(Side.LEFT, Side.RIGHT);
-            default:
-                throw new RuntimeException("Could not determine " + "intersection between " + this + " and " + other);
+        case 0: // 0000
+        case 15: // 1111
+            return EnumSet.allOf(Side.class);
+        case 8: // 1000
+        case 11: // 1011
+            return EnumSet.of(Side.RIGHT);
+        case 4: // 0100
+        case 7: // 0111
+            return EnumSet.of(Side.LEFT);
+        case 2: // 0010
+        case 14: // 1110
+            return EnumSet.of(Side.BOTTOM);
+        case 1: // 0001
+        case 13: // 1101
+            return EnumSet.of(Side.TOP);
+        case 10: // 1010
+            return EnumSet.of(Side.RIGHT, Side.BOTTOM);
+        case 9: // 1001
+            return EnumSet.of(Side.RIGHT, Side.TOP);
+        case 6: // 0110
+            return EnumSet.of(Side.LEFT, Side.BOTTOM);
+        case 5: // 0101
+            return EnumSet.of(Side.LEFT, Side.TOP);
+        case 12: // 1100
+            return EnumSet.of(Side.TOP, Side.BOTTOM);
+        case 3: // 0011
+            return EnumSet.of(Side.LEFT, Side.RIGHT);
+        default:
+            throw new RuntimeException("Could not determine " + "intersection between " + this + " and " + other);
         }
-        
+
     }
-    
+
     public float getX() {
         return x;
     }
-    
+
     public float getY() {
         return y;
     }
-    
+
     public Vector2f getPosition() {
         return new Vector2f(x, y);
     }
-    
+
     public Rect setPosition(float x, float y) {
         return new Rect(x, y, width, height);
     }
-    
+
     public float getHeight() {
         return height;
     }
-    
+
     public float getWidth() {
         return width;
     }
-    
+
     public float getMaxX() {
         return x + width;
     }
-    
+
     public float getMaxY() {
         return y + height;
     }
-    
+
     public Shape toShape() {
         return new Rectangle(x, y, width, height);
     }
-    
+
     @Override
     public int hashCode() {
         final int prime = 31;
@@ -250,10 +257,10 @@ public class Rect {
         result = prime * result + Float.floatToIntBits(y);
         return result;
     }
-    
+
     @Override
     public boolean equals(Object obj) {
-        
+
         if (this == obj)
             return true;
         if (obj == null)
@@ -271,10 +278,10 @@ public class Rect {
             return false;
         return true;
     }
-    
+
     @Override
     public String toString() {
         return "Rect [x=" + x + ", y=" + y + ", h=" + height + ", w=" + width + "]";
     }
-    
+
 }
--- Platformer/src/com/gravity/physics/PhysicsFactory.java
+++ Platformer/src/com/gravity/physics/PhysicsFactory.java
@@ -9,9 +9,10 @@ public final class PhysicsFactory {
     private static final float DEFAULT_GRAVITY = 1.0f / 750f;
     public static final float DEFAULT_REHANDLE_BACKSTEP = -15f;
     public static final float DEFAULT_OFFSET_GROUND_CHECK = 2f;
+    public static final float DEFAULT_ALLOWED_SIDE_OVERLAP = 0.5f;
 
     public static GravityPhysics createDefaultGravityPhysics(CollisionEngine engine) {
-        return new GravityPhysics(engine, DEFAULT_GRAVITY, DEFAULT_REHANDLE_BACKSTEP, DEFAULT_OFFSET_GROUND_CHECK);
+        return new GravityPhysics(engine, DEFAULT_GRAVITY, DEFAULT_REHANDLE_BACKSTEP, DEFAULT_OFFSET_GROUND_CHECK, DEFAULT_ALLOWED_SIDE_OVERLAP);
     }
 
     public static SimplePhysics createSimplePhysics() {

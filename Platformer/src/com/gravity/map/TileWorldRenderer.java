da2d49b1b282899b2160ce7e4a1909834ddc96f1
dxiao <dxiao@mit.edu>
2012-11-22 01:46:44 -0500

    Added assets for facelifting

--- Platformer/src/com/gravity/map/TileWorldRenderer.java
+++ Platformer/src/com/gravity/map/TileWorldRenderer.java
@@ -15,32 +15,16 @@ import com.gravity.root.Renderer;
 
 public class TileWorldRenderer implements Renderer {
     private TileWorld tileMap;
-    private Image Background1;
-    private Image Background2;
-    private Image Background3;
-    private Image Background4;
-    private Image lastBackGround;
-    private List<Image> backgrounds;
-    int tweener;
+    private Image background;
     private List<Renderer> extraRenderers;
 
     public TileWorldRenderer(TileWorld tileMap) {
         this.tileMap = tileMap;
         try {
-            // "./assets/BambooForest2small.png"
-            // "./assets/2ufa5h5.png"
-            Background1 = new Image("./assets/FrogAssets/frogtower.png");
-            Background2 = new Image("./assets/FrogAssets/frogtower1.png");
-            Background3 = new Image("./assets/FrogAssets/frogtower2.png");
-            Background4 = new Image("./assets/FrogAssets/frogtower3.png");
+            background = new Image("assets/background-no-shelf.png");
         } catch (SlickException e) {
             throw new RuntimeException(e);
         }
-        backgrounds = Lists.newArrayList();
-        backgrounds.add(Background1);
-        backgrounds.add(Background2);
-        backgrounds.add(Background3);
-        backgrounds.add(Background4);
         extraRenderers = Lists.newArrayList();
         for (TriggeredText tt : tileMap.getTriggeredTexts()) {
             extraRenderers.add(new TriggeredTextRenderer(tt));
@@ -52,21 +36,7 @@ public class TileWorldRenderer implements Renderer {
 
     @Override
     public void render(Graphics g, int offsetX, int offsetY) {
-        if (tweener % 15 == 0) {
-            if (lastBackGround == null) {
-                g.drawImage(Background1, 0, 0);
-                lastBackGround = Background1;
-            } else {
-                g.drawImage((backgrounds.get(backgrounds.indexOf(lastBackGround) + 1)), 0, 0);
-                lastBackGround = backgrounds.get(backgrounds.indexOf(lastBackGround) + 1);
-                if (lastBackGround == Background4) {
-                    lastBackGround = Background1;
-                }
-            }
-        } else {
-            g.drawImage(lastBackGround, 0, 0);
-        }
-        tweener++;
+        g.drawImage(background, 0, 0);
 
         // TiledMap supports easy rendering. Let's use it!
         // Later we'll need to some how adjust x,y for offset/scrolling

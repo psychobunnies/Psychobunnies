ef5210eedf59b1a2a92e04f6a6654d1d104e24bb
Ziad R Baaklini <ziadrbaaklini@gmail.com>
2012-11-22 22:48:35 -0500

    sfsf

--- Platformer/src/com/gravity/root/PlatformerGame.java
+++ Platformer/src/com/gravity/root/PlatformerGame.java
@@ -29,12 +29,10 @@ public class PlatformerGame extends StateBasedGame {
             new LevelInfo("Split World", "Slingshot Fun", "assets/Levels/split_world.tmx", 1002),
             new LevelInfo("Test Moving", "", "assets/Levels/moving_test.tmx", 1005),
             new LevelInfo("Bouncy 1", "A first brush with bouncing", "assets/Levels/Bouncy_1.tmx", 1006),
-            new LevelInfo("Checkpointing", "", "assets/Levels/checkpointing.tmx", 1007),
             new LevelInfo("Falling", "So you thought you understood gravity...", "assets/Levels/falling.tmx", 1008),
-            new LevelInfo("Level 1", "Staircases are hard!", "assets/Levels/game_map_final.tmx", 1003),
-            new LevelInfo("Level 2", "More traditional Mario", "assets/Levels/level2.tmx", 1004),
-            new LevelInfo("Shortcuts", "Timetest", "assets/levels/shortcuts.tmx", 1009),
-            new LevelInfo("Test Stomps", "", "assets/Levels/checkpointing.tmx", 1010)
+            new LevelInfo("Test Stomps", "", "assets/Levels/checkpointing.tmx", 1007),
+            new LevelInfo("Shortcuts", "Timetest", "assets/Levels/shortcuts.tmx", 1010),
+            new LevelInfo("Elevators","","assets/levels/Elevators.tmx",1011)
     };
     //@formatter:on
 

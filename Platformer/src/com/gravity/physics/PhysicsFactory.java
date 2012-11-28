05b20e3982c8071ce4e99ea27609abb53017fc25
pgruevski <predrag_gruevski@yahoo.com>
2012-11-28 14:57:59 -0500

    Made moving tile feather distance smaller to make movement fluid.

--- Platformer/src/com/gravity/physics/PhysicsFactory.java
+++ Platformer/src/com/gravity/physics/PhysicsFactory.java
@@ -12,7 +12,7 @@ public final class PhysicsFactory {
     public static final float DEFAULT_FRICTION_ACCEL_RATIO = 50f;
     public static final float DEFAULT_REHANDLE_BACKSTEP = -15f;
     public static final float DEFAULT_OFFSET_GROUND_CHECK = 5f;
-    public static final float DEFAULT_MOVING_FEATHER = 2f;
+    public static final float DEFAULT_MOVING_FEATHER = 0.5f;
 
     public static GravityPhysics createDefaultGravityPhysics(CollisionEngine engine) {
         return new GravityPhysics(engine, DEFAULT_GRAVITY, DEFAULT_REHANDLE_BACKSTEP, DEFAULT_OFFSET_GROUND_CHECK, DEFAULT_GROUND_FRICTION,

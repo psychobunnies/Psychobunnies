3b834172e89778ec81ecf83c18bc0ce766f565e4
dxiao <dxiao@mit.edu>
2012-11-30 19:54:39 -0500

    Somebody decided to revert my commit to fix MainMenuState...

--- Platformer/src/com/gravity/root/CreditsState.java
+++ Platformer/src/com/gravity/root/CreditsState.java
@@ -1,26 +1,37 @@
 package com.gravity.root;
 
+import java.awt.Color;
+import java.awt.Font;
+
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.UnicodeFont;
+import org.newdawn.slick.font.effects.ColorEffect;
 import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
-
 public class CreditsState extends BasicGameState {
-    
+
     static public final int ID = 5;
     private StateBasedGame game;
     private Rectangle MenuButton;
-    
+    private UnicodeFont font;
+
     @Override
     public void init(GameContainer container, StateBasedGame game) throws SlickException {
         this.game = game;
+        Font awtFont = new Font("Arial", Font.BOLD, 14);
+        font = new UnicodeFont(awtFont);
+        font.getEffects().add(new ColorEffect(Color.white));
+        font.addAsciiGlyphs();
+        font.loadGlyphs();
     }
-    
+
     @Override
     public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
+        g.setFont(font);
         g.drawString("CREDITS", 50, 75);
         g.drawString("Music by Matthew Pablo", 50, 125);
         g.drawString("Sound effects were made by Zachary Segal", 50, 150);
@@ -30,25 +41,25 @@ public class CreditsState extends BasicGameState {
         g.drawString("Graphics and rendering designed and implemented by Chris Dessonville and Zachary Segal", 50, 250);
         g.drawString("Assets from 'The Independent Gaming Source's Assemblee Competition' were used", 50, 275);
         g.drawString(" with permission from the authors.", 50, 300);
-        
+
         g.draw(MenuButton = new Rectangle(48, 383, 100, 48));
         g.drawString("Back", 50, 385);
     }
-    
+
     @Override
     public void update(GameContainer arg0, StateBasedGame arg1, int arg2) throws SlickException {
     }
-    
+
     @Override
     public void mouseClicked(int button, int x, int y, int clickCount) {
         if (MenuButton.contains(x, y)) {
             game.enterState(MainMenuState.ID);
         }
     }
-    
+
     @Override
     public int getID() {
         return ID;
     }
-    
+
 }

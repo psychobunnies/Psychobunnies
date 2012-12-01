5060963bee4189ba462165bfe37cbc26248e8e62
Patrick Hulin <patrick.hulin@gmail.com>
2012-11-30 20:14:32 -0500

    move checkpoint 4 on bouncy 2

--- Platformer/src/com/gravity/root/GameSounds.java
+++ Platformer/src/com/gravity/root/GameSounds.java
@@ -26,7 +26,8 @@ public final class GameSounds {
                     new Sound("./assets/Sound/jump_ha.ogg"),
                     new Sound("./assets/Sound/jump_ho.ogg"),
                     new Sound("./assets/Sound/jump_woo.ogg"),
-                    new Sound("./assets/Sound/jump_hup.ogg"));
+                    new Sound("./assets/Sound/jump_hup.ogg"),
+                    new Sound("./assets/Sound/yippee.wav"));
             slingshotSounds = Lists.newArrayList(
                     new Sound("./assets/Sound/slingshot_1.ogg"),
                     new Sound("./assets/Sound/slingshot_2.ogg"),

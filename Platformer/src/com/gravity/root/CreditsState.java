3e4da319aac54f9ea7ea1587c3503d052d59dbcf
dxiao <dxiao@mit.edu>
2012-11-30 21:25:02 -0500

    Made the credits page baller

--- Platformer/src/com/gravity/root/CreditsState.java
+++ Platformer/src/com/gravity/root/CreditsState.java
@@ -5,9 +5,11 @@ import java.awt.Font;
 
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.UnicodeFont;
 import org.newdawn.slick.font.effects.ColorEffect;
+import org.newdawn.slick.font.effects.ShadowEffect;
 import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
@@ -18,19 +20,23 @@ public class CreditsState extends BasicGameState {
     private StateBasedGame game;
     private Rectangle MenuButton;
     private UnicodeFont font;
+    private Image background;
 
     @Override
     public void init(GameContainer container, StateBasedGame game) throws SlickException {
         this.game = game;
         Font awtFont = new Font("Arial", Font.BOLD, 14);
         font = new UnicodeFont(awtFont);
+        font.getEffects().add(new ShadowEffect(Color.black, 2, 2, 0.5f));
         font.getEffects().add(new ColorEffect(Color.white));
         font.addAsciiGlyphs();
         font.loadGlyphs();
+        background = new Image("assets/background-no-shelf.png");
     }
 
     @Override
     public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
+        g.drawImage(background, 0, 0);
         g.setFont(font);
         g.drawString("CREDITS", 50, 75);
         g.drawString("Music by Matthew Pablo", 50, 125);
